42bf464b218b29198c5974ec7e5a8d8d4a59d2e4
Sergey Yavnyi <blacktaxi@gmail.com>
2014-02-22 01:27:35 +0200

    Initial.

---
+++ src/ObservableProperty/ObservableProperty.Test/Tests.fs
@@ -0,0 +1,36 @@
+﻿namespace System.Reactive.Properties.Test
+
+open Xunit
+
+open System
+open System.Reactive
+open System.Reactive.Linq
+
+open System.Reactive.Properties
+open System.Reactive.Properties.Operators
+open System.Reactive.Properties.Extensions
+
+type Tests () =
+    [<Fact>]
+    let ``a property should correctly return the stored value`` () =
+        let p = new ObservableProperty<_>()
+
+        p <<- 5
+        Assert.True(!!p = 5)
+
+        p <<- 6
+        Assert.True(!!p = 6)
+
+    [<Fact>]
+    let ``property changes should be observed correctly`` () =
+        let p = new ObservableProperty<_>()
+        let o = p.ObserveChanges.Replay()
+        o.Connect() |> ignore
+
+        p <<- 1
+        p <<- 2
+        p <<- 3
+
+        (p :> IDisposable).Dispose()
+
+        Assert.Equal<int[]>([| 1; 2; 3 |], o.ToArray().First())
---
+++ src/ObservableProperty/ObservableProperty/ObservableProperty.fs
@@ -0,0 +1,80 @@
+﻿namespace System.Reactive.Properties
+
+open System
+open System.Reactive
+open System.Reactive.Concurrency
+open System.Reactive.Disposables
+open System.Reactive.Subjects
+open System.Reactive.Linq
+
+type IReadableProperty<'a> =
+    abstract Value : 'a
+    abstract Observe : IObservable<'a>
+
+type IWriteableProperty<'a> =
+    abstract Set : 'a -> unit
+    abstract Provide : IObserver<'a>
+
+type ObservableProperty<'a>(?initialValue) =
+    let subject = new BehaviorSubject<_>(defaultArg initialValue Unchecked.defaultof<_>)
+    let observable = subject.DistinctUntilChanged()
+
+    [<VolatileField>]
+    let mutable currentValue : 'a = Unchecked.defaultof<_>
+
+    let updateCurrentValue =
+        subject.ObserveOn(Scheduler.Immediate).Subscribe(fun x -> currentValue <- x)
+
+    let toDispose =
+        new CompositeDisposable(subject :> IDisposable, updateCurrentValue)
+
+    let dispose disposing =
+        if disposing then
+            subject.OnCompleted()
+            toDispose.Dispose()
+
+    interface IReadableProperty<'a> with
+        member this.Value = currentValue
+        member this.Observe = observable
+
+    interface IWriteableProperty<'a> with
+        member this.Set(x) = subject.OnNext(x)
+        member this.Provide = subject :> _
+
+    interface IDisposable with
+        member this.Dispose() = dispose true
+
+type ObservablePropertyFromObservable<'a>(source : IObservable<'a>) =
+    let inner = new ObservableProperty<'a>()
+
+    let subscription =
+        source.ObserveOn(Scheduler.Immediate).Subscribe((inner :> IWriteableProperty<_>).Provide)
+
+    let dispose disposing =
+        if disposing then
+            subscription.Dispose()
+            (inner :> IDisposable).Dispose()
+
+    interface IReadableProperty<'a> with
+        member this.Value = (inner :> IReadableProperty<_>).Value
+        member this.Observe = (inner :> IReadableProperty<_>).Observe
+
+    interface IDisposable with
+        member this.Dispose() = dispose true
+
+module Operators =
+    let inline (<<-) (p : IWriteableProperty<'a>) (x : 'a) = p.Set(x)
+    let inline (!!) (p : IReadableProperty<'a>) : 'a = p.Value
+
+module Extensions =
+    type IReadableProperty<'a> with
+        member property.ObserveChanges = property.Observe.Skip(1)
+
+    type IObservable<'a> with
+        member observable.AsProperty () = new ObservablePropertyFromObservable<'a>(observable)
+
+    type IObserver<'a> with
+        member observer.AsProperty () =
+            { new IWriteableProperty<'a> with
+                member this.Set(x) = observer.OnNext(x)
+                member this.Provide = observer }
