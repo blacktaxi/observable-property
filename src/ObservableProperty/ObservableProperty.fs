c1e9ab9761cfa40bb0ed80f1bef7a509ce80d873
Sergey Yavnyi <blacktaxi@gmail.com>
2014-05-05 12:14:39 +0300

    Lock reads and writes to currentValue.

--- src/ObservableProperty/ObservableProperty.fs
+++ src/ObservableProperty/ObservableProperty.fs
@@ -28,12 +28,12 @@ type ObservableProperty<'a>(initialValue) =
     let subject = new Subject<_>()
     let observable = subject :> IObservable<_>
 
-    // @TODO is this really ok?
-    [<VolatileField>]
+    let gate = obj()
     let mutable currentValue : 'a = initialValue
 
     let updateCurrentValue =
-        subject.ObserveOn(Scheduler.Immediate).Subscribe(fun x -> currentValue <- x)
+        subject.ObserveOn(Scheduler.Immediate).Subscribe(
+            fun x -> lock gate <| fun _ -> currentValue <- x)
 
     let toDispose =
         new CompositeDisposable(subject :> IDisposable, updateCurrentValue)
@@ -46,7 +46,7 @@ type ObservableProperty<'a>(initialValue) =
     new() = ObservableProperty(Unchecked.defaultof<_>)
 
     interface IReadWriteProperty<'a> with
-        member this.Value = currentValue
+        member this.Value = lock gate <| fun _ -> currentValue
         member this.WhenValueSet = observable
 
         member this.Set(x) = subject.OnNext(x)
